69ff442ce21f904a3c16bf288de938550f72636c
janelle-and <janelle.andrews@rdpolytech.ca>
2024-12-16 20:52:34 -0700

    update to form/poll

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,8 @@
+let MonaValue = 4;
+let MaxwellValue = 7;
+let TenleyValue = 6;
+let RockyValue = 5;
+
 function selectFavoritePet(){
     let user_Name = document.getElementById("userName").value;
 
@@ -5,16 +10,20 @@ function selectFavoritePet(){
         if(document.getElementById("radio1").checked){
             // let user_Name = document.getElementById("userName").value;
             let radio_1 = document.getElementById("radio1_value").textContent;
-            document.getElementById('display').innerHTML = "Hello, " + user_Name + ". " + "Your prefered pet is: " + radio_1;
+            MaxwellValue = MaxwellValue + 1;
+            displayChart()
         } else if(document.getElementById("radio2").checked){
             let radio_2 = document.getElementById("radio2_value").textContent;
-            document.getElementById('display').innerHTML = "Hello, " + user_Name + ". " + "Your prefered pet is: " + radio_2;
+            MonaValue = MonaValue + 1;
+            displayChart()
         } else if(document.getElementById("radio3").checked){
             let radio_3 = document.getElementById("radio3_value").textContent;
-            document.getElementById('display').innerHTML = "Hello, " + user_Name + ". " + "Your prefered pet is: " + radio_3;
+            RockyValue = RockyValue + 1;
+            displayChart()
         } else if(document.getElementById('radio4').checked){
             let radio_4 = document.getElementById("radio4_value").textContent;
-            document.getElementById('display').innerHTML = "Hello, " + user_Name + ". " + "Your prefered pet is: " + radio_4;
+            TenleyValue = TenleyValue + 1;
+            displayChart()
         } else { 
             alert("Please, select your prefered pet!");   
         }
@@ -27,4 +36,25 @@ function clearForm() {
     document.getElementsByName("Choose").checked = false;
     document.getElementById("userName").value = "";
 
+}
+
+function displayChart(){
+    $('#selectform').css('visibility', 'hidden');
+    new Chart("myChart", {
+        type: "bar",
+        data: {
+        labels: ["Mona","Tenley","Maxwell","Rocky"],
+        datasets: [{
+            backgroundColor: ['DarkMagenta','SaddleBrown','DarkRed','LightSlateGrey'],
+            data: [MonaValue,TenleyValue,MaxwellValue,RockyValue]
+        }]
+        },
+        options: {
+        legend: {display: false},
+        title: {
+            display: true,
+            text: "Favourite Pets"
+        }
+        }
+    });
 }
